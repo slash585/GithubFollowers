eecb7aaff89d8b9a7c6e034697ce20bc40c217cd
slash585 <slsh585@gmail.com>
2023-07-21 23:41:51 +0300

    feat: create custom textfield and button view

---
+++ Github Followers/Github Followers/GFButton.swift
@@ -0,0 +1,33 @@
+//
+//  GFButton.swift
+//  Github Followers
+//
+//  Created by Mehmet Ali Özdemir on 21.07.2023.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
