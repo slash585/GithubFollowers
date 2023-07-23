2579c3c5e630c8bae765c0bd8a9a0e5a7bd20876
slash585 <slsh585@gmail.com>
2023-07-23 03:33:07 +0300

    feat: move createThreeColumnFlowLayout func to UIHelpers

--- Github Followers/Github Followers/Screens/FollowerViewController.swift
+++ Github Followers/Github Followers/Screens/FollowerViewController.swift
@@ -55,27 +55,14 @@ final class FollowerViewController: UIViewController {
     }
     
     private func configureCollectionView(){
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelpers.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
         
     }
     
-    private func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout{
-        
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - ( padding * 2 ) - ( minimumItemSpacing * 2 )
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
-    }
+
     
     private func configureDataSource(){
         dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
