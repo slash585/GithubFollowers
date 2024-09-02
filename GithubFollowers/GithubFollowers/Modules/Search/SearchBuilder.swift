a7f388eb6371eb412579bab23b07b09b5afc0aa3
slash585 <slsh585@gmail.com>
2024-09-02 18:41:35 +0300

    Init app

---
+++ GithubFollowers/GithubFollowers/Modules/Search/SearchBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  SearchBuilder.swift
+//  GithubFollowers
+//
+//  Created by Mehmet Ali Özdemir on 2.09.2024.
+//
+
+import UIKit
+
+final class SearchBuilder {
+    static func make() -> SearchViewController {
+        let viewController = SearchViewController()
+        return viewController
+    }
+}
---
+++ GithubFollowers/GithubFollowers/Modules/Search/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Mehmet Ali Özdemir on 2.09.2024.
+//
+
+import UIKit
+
+final class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
